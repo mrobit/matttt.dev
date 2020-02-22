2a6d29ee258989b823003bc78894dbcaa2e95935
Matt Robitaille <matt@anchour.com>
2020-02-21 23:54:32 -0500

    Initial commit.

---
+++ source/_layouts/master.blade.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="description" content="{{ $page->meta_description ?? $page->siteDescription }}">
+
+        <meta property="og:title" content="{{ $page->title ?  $page->title . ' | ' : '' }}{{ $page->siteName }}"/>
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="{{ $page->getUrl() }}"/>
+        <meta property="og:description" content="{{ $page->siteDescription }}" />
+
+        <title>{{ $page->title ?  $page->title . ' | ' : '' }}{{ $page->siteName }}</title>
+
+        <link rel="home" href="{{ $page->baseUrl }}">
+        <link rel="icon" href="/favicon.ico">
+        <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans:400,700&display=swap" rel="stylesheet">
+        <link href="/blog/feed.atom" type="application/atom+xml" rel="alternate" title="{{ $page->siteName }} Atom Feed">
+
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-54192412-1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-54192412-1');
+        </script>
+
+
+        @stack('meta')
+
+        @if ($page->production)
+            <!-- Insert analytics code here -->
+        @endif
+
+        <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,300i,400,400i,700,700i,800,800i" rel="stylesheet">
+        <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
+    </head>
+
+    <body class="flex flex-col justify-between min-h-screen leading-normal font-sans">
+        <header class="flex items-center bg-blue-900 text-white border-b" role="banner">
+            <div class="container flex items-center mx-auto p-4">
+                <div class="flex items-center">
+                    <a href="/" title="{{ $page->siteName }} home" class="inline-flex items-center">
+                        <h1 class="text-lg md:text-2xl font-semibold text-white hover:text-blue-200 my-0">{{ $page->siteName }}</h1>
+                    </a>
+                </div>
+
+                <div id="vue-search" class="flex flex-1 justify-end items-center">
+                    {{-- <search></search> --}}
+
+                    @include('_nav.menu')
+
+                    @include('_nav.menu-toggle')
+                </div>
+            </div>
+        </header>
+
+        @include('_nav.menu-responsive')
+
+        <main role="main" class="flex-auto w-full container mx-auto py-16 px-4">
+            @yield('body')
+        </main>
+
+        <footer class="bg-white text-xs mt-12 py-4" role="contentinfo">
+            <ul class="list-none container mx-auto px-4">
+                <li class="md:mr-2">
+                    &copy; Matt Robitaille / matttt.dev {{ date('Y') }}.
+                </li>
+            </ul>
+        </footer>
+
+        <script src="{{ mix('js/main.js', 'assets/build') }}"></script>
+
+        @stack('scripts')
+    </body>
+</html>
